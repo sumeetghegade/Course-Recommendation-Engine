d216910910dd4d4112b2614af32b8240c9cbac2e
T.K. Bui <thuy-khuong.bui@sjsu.edu>
2022-05-12 00:31:25 -0700

    Added function from login.html's login button to the functions.js page to make code cleaner. Made simple edits to login.html and signup.html.

--- CourseRecommender/src/main/resources/static/js/functions.js
+++ CourseRecommender/src/main/resources/static/js/functions.js
@@ -4,11 +4,18 @@
 // https://stackoverflow.com/questions/16562577/how-can-i-make-a-button-redirect-my-page-to-another-page
 function goLogin() {
     location.href="/v1/login";
-    console.log("clicked login");
+    console.log("clicked login from index page");
 }
 
 // navigating to signup page
 function goSignup() {
     location.href="/v1/signup";
-    console.log("clicked signup");
+    console.log("clicked signup from index page");
+}
+
+// for when clicking login on login page
+function login() {
+    location.reload();
+    location.href='/v1/home/' + document.getElementById('username').value;
+    console.log("clicked login from login page");
 }
